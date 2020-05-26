f31e951cdf8b06b4aee7b3a633a8cff7f3059047
yangming <yangming@shein.com>
2020-05-26 16:12:57 +0800

    add TestFunction

---
+++ src/sniff/TestFunction.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm
+ * Author yangming <yangming@shein.com>
+ * Date: 2020/5/26
+ * Time: 16:09
+ */
+
+namespace \ComposerTest\Sniff;
+
+class TestFunction
+{
+    function show(){
+        return "hello,world!";
+    }
+
+}
